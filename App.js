01a28d3dc2d21506d2cb11449887e358d722f64f
kingsman44 <shivan.0972@gmail.com>
2024-09-04 01:40:56 +0530

    init project

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
+import "./global.css"
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View className="flex-1 bg-[#fff] items-center justify-center">
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
